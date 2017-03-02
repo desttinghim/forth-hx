a6f8346e8ab9da2c0939861ce2c57faf184ab8ac
Louis Pearson <desttinghimgame@gmail.com>
2017-03-02 13:49:50 -0600

    Making it better...

--- src/Concat.hx
+++ src/Concat.hx
@@ -4,36 +4,85 @@ enum StackType {
   String;
 }
 
+typedef DictEntry = {
+  var word:String;
+  var native:Bool;
+  var immediate:Bool;
+  var func:Void->Void;
+  var def:Array<Int>;
+};
+
 class Concat {
 
-  public var words:Map<String,Void->Void>;
+  public var words:Array<DictEntry>;
+  public var tokens:Array<String>;
   public var stack:Array<{value:Dynamic,type:StackType}>;
   public var rStack:Array<{value:Dynamic,type:StackType}>;
 
-  public function compile(str) {
-    var tokens = tokenize(str);
-    for (token in tokens) {
-      trace(token);
-      var x = getWord(token);
-      if (x != null) {
-        x();
+  public function run() {
+    while(true) {
+      if (tokens.length == 0)  getInput();
+      var token:String = nextToken();
+      if (token==null) continue;
+      var xword = null;
+      for (word in words) {
+        if (word.word == token) xword = word;
+      }
+      if (xword!=null)  {
+        if (xword.native) xword.func();
+        else              interpWords(xword.def);
+      } else {
+        push(Std.parseInt(token), Int);
       }
     }
   }
 
-  public function new() {
-    words = new Map<String,Void->Void>();
-    stack = [];
-    rStack = [];
-    addWord("print", function() trace(stack.pop().value));
+  public function getInput() {
+    tokens = tokens.concat(tokenize(Sys.stdin().readLine()));
   }
 
-  public function addWord(name, f) {
-    words.set(name, f);
+  public function nextToken() {
+    return tokens.shift();
+  }
+
+  public function new() {
+    tokens = [];
+    words = [];
+    stack = [];
+    rStack = [];
+    addWord("+", function() push(pop().value + pop().value, Int));
+    addWord("-", function() push(pop().value - pop().value, Int));
+    addWord("*", function() push(pop().value * pop().value, Int));
+    addWord("/", function() push(pop().value / pop().value, Int));
+    addWord("print", function() Sys.print(pop().value));
+    addWord("clear", function() Sys.command("clear"));
+  }
+
+  public function addWord(word, func, ?def=null) {
+    words.push({
+      word:word,
+      native:false,
+      immediate:false,
+      func:func,
+      def: def==null?[]:def,
+    });
+  }
+
+  public function interpWords(tokens:Array<Int>) {
+    for (token in tokens) interpOneWord(token);
+  }
+
+  public function interpOneWord(xtoken) {
+    if (words[xtoken].native) words[xtoken].func();
+    else                      interpWords(words[xtoken].def);
   }
 
   public function push(val, type) {
     stack.push({value:val, type:type});
+  }
+
+  public function pop() {
+    return stack.pop();
   }
   // Steps:
   // 1. Tokenize
@@ -58,19 +107,19 @@ class Concat {
     }
     return strCopy;
   }
-  // 2. Look up the word, put value on stack or push value on stack
-  public function getWord(str:String) {
-    if(str.charAt(0) == '"') {
-      push(str, String);
-      return null;
-    }
-    else if(Std.parseInt(str) != null) {
-      push(str, Int);
-      return null;
-    }
-    else if(words.get(str) != null) {
-      return words.get(str);
-    }
-    return null;
-  }
+  // // 2. Look up the word, put value on stack or push value on stack
+  // public function getWord(str:String) {
+  //   if(str.charAt(0) == '"') {
+  //     push(str, String);
+  //     return null;
+  //   }
+  //   else if(Std.parseInt(str) != null) {
+  //     push(str, Int);
+  //     return null;
+  //   }
+  //   else if(words.get(str) != null) {
+  //     return words.get(str);
+  //   }
+  //   return null;
+  // }
 }
--- src/Main.hx
+++ src/Main.hx
@@ -4,6 +4,6 @@ import sys.io.File;
 class Main {
   static function main() {
     // new Concat.compile(File.getContent(Sys.args()[0]));
-    new Concat().compile('"Hello, World!" print');
+    new Concat().run( /*'"Hello, World!" print'*/ );
   }
 }
